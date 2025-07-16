f267740233b0945a92bac2d2624a5e0eaa521807
sboyarkin <sergey.boyarkin@mail.ru>
2025-07-16 11:09:03 +0500

    initiall commit

---
+++ src/componets/ShopItemFunc/ShopItemFunc.jsx
@@ -0,0 +1,20 @@
+export const ShopItemFunc = ({item, itemImg}) => {
+    return (
+        <>
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">
+                    {item.descriptionFull}
+                </div>
+                <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+                <div className="divider"></div>
+                <div class="purchase-info">
+                    <div class="price">{item.currency}{item.price}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        </>
+    )
+}
